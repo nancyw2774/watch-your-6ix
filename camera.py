8fef8f02c3f06cb5730a32379234b0e3f6ee279d
Pi <nancyw2774@gmail.com>
2024-02-16 12:02:40 -0500

    camera test

--- camera.py
+++ camera.py
@@ -3,6 +3,7 @@ import time
 
 camera = PiCamera()
 
-camera.start_preview()
-
-camera.capture("test2.jpg")
+camera.resolution = (640, 480)
+camera.start_recording("test_video.h264")
+camera.wait_recording(60)
+camera.stop_recording()
