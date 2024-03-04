9b751a1dc4239f041cb820c6c125facf7e80ec4d
Ryan20010115 <c_siyuan2001@hotmail.com>
2024-03-04 15:58:33 -0500

    hazard classification

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -7,37 +7,51 @@ constant loop that monitors incoming data and determines whether a threat is det
 when a hazard condition is satisfied, call external function to call CV + send info to phone
 '''
 import time
+from kld7 import KLD7, KLD7Exception
+import time
+import traceback
+import subprocess
+
+class kld7_class:
+
+    # radar set to None on failure to init
+    def __init__(self):
+        self.radar = None
+        port = "/dev/ttyAMA0"
+        
+        try:
+            self.radar = KLD7(port, baudrate=115200)
+        except Exception as e:
+            print(f"An unexpected error occurred while initializing the KLD7 radar: {e}")
+
+            # see if granting permissions fixes issue (catching permission error directly is not working)
+            try:
+                print(f"Granting permissions for {port}")
+                subprocess.run(["sudo", "chmod", "g+r", port], check=True)
+                self.radar = KLD7(port, baudrate=115200)
+            except Exception as pe:
+                print(f"Error setting permissions for {port}: {pe}")
 
 def main():
     '''
     setup input sources
     '''
- 
+    sensor = kld7_class().radar
 
     while True:
-        bike_speed = "BIKE SPEED"
-        bike_dir = "BIKE DIR"
-        radar = "PLACEHOLDER"
-        imu = "PLACEHOLDER"
+        detection_data = sensor.read_TDAT()
         # outlining cases to take action
         #case 1: bike is moving, object detected
-        if bike_speed > 5:
-            if radar < 25: #max 25m distance to trigger alerts
-                time.sleep(0.1)
-                temp = # sample new radar distance
-                if abs(temp-radar) > 10:
-                    continue #sample again, big change in reading implies an erroneous reading
-                if temp - radar <= 0: #object distance non-increasing
-                    if temp-radar < 15:
-                        continue # object closing very fast, warn immediate while processing cv
-                    continue
-                    #call CV to verify object identity
+
+        # add bike speed if statement here
+        if detection_data.speed < 0:
+            if detection_data.distance < 25: #max 25m distance to trigger alerts
+                #call CV to verify object identity
+                print("hazard moving closer")
+                continue
         else:
-            if radar < 25:
-                time.sleep(0.1)
-                temp = # sample new radar distance
-                if abs(temp-radar) > 10:
-                    continue #sample again, big change in reading implies an erroneous reading
-                if temp - radar < 15: #object object is closing at fast, speed, call cv but warn immediately
-                    continue
-                    #call CV to verify object identity
+            if detection_data.distance < 10:
+                #call CV to verify object identity
+                print("hazard is really close")
+                continue
+        #else: if bike is not moving, hazard when speed < -20
