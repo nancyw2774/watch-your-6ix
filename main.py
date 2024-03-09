c9eb7ccd687143c765e4eff6b69423434b4bc890
Jackie Zhu <jackiexu829@gmail.com>
2024-03-09 12:00:51 -0500

    update main

--- main.py
+++ main.py
@@ -10,11 +10,10 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 
 def gen_frames(): 
-    picam2 = Picamera2()
-    picam2.start()
+    cam.start()
     start_time = time.time()
     while time.time() - start_time < 30:
-        frame = picam2.capture_array()
+        frame = cam.capture_array()
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
